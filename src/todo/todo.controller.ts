0d9d0b26cacdabc51e97631624ccb0b043120b79
PrintHolamundo <jdohca@gmail.com>
2023-06-03 11:25:34 -0600

    Selecionar TODOS

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Controller('todo')
 export class TodoController {
@@ -18,8 +19,8 @@ export class TodoController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.todoService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number): Todo {
+    return this.todoService.findOne(id);
   }
 
   @Patch(':id')
