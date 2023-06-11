fac98ff7763d07a201a66c56df64e446c7c1f0d4
PrintHolamundo <jdohca@gmail.com>
2023-06-11 15:48:08 -0600

    Instalar apollo Studio

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,17 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { TodoModule } from './todo/todo.module';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     TodoModule,
     HelloWorldModule,
