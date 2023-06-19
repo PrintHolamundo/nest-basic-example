32532e149828d5e9825cb8c950483b5a7f82203e
PrintHolamundo <jdohca@gmail.com>
2023-06-18 22:56:54 -0600

    Generar un nuevo Query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 import { HelloWorldModule } from './hello-world.module';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { description: 'Hola mundo es lo que retorna', name: 'hello' })
   HelloWorld(): string {
     return 'Hola Mundo';
+  }
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
